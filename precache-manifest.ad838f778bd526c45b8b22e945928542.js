c06259b6ce2769275bec343f4d793ce316bbb89e
sandysanthosh <sandysanthosh101@gmail.com>
2020-06-07 17:52:25 +0530

    Updates

---
+++ precache-manifest.ad838f778bd526c45b8b22e945928542.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66c800ba33c162d76b38c4cf4f46f52b",
+    "url": "/fdapp/index.html"
+  },
+  {
+    "revision": "0cf8192591687862b302",
+    "url": "/fdapp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fb2b7d1189c2a22796fe",
+    "url": "/fdapp/static/js/2.80eb9ca4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fdapp/static/js/2.80eb9ca4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0cf8192591687862b302",
+    "url": "/fdapp/static/js/main.207712ea.chunk.js"
+  },
+  {
+    "revision": "ecafec3e5d09c4afbc34",
+    "url": "/fdapp/static/js/runtime-main.981f1aad.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/fdapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
