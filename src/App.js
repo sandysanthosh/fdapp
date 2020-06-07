5f7af6ae09452667ed26216dd5027c3d34ef0c7d
sandysanthosh <sandysanthosh101@gmail.com>
2020-06-07 17:42:27 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+    <h1>hello</h1>
+    <p>First App</p> 
+    </div>
+  );
+}
+
+export default App;
